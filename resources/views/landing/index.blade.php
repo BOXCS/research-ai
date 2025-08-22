fe235b24f55fe3ac564ee4364359d230b1b2518d
Zaky <zakyabdurt@gmail.com>
2025-08-22 14:37:18 +0700

    change icon in blade

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -20,6 +20,8 @@
     <!-- Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
 
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
